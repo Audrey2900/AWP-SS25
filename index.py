cc051ae3498792f35a84c4731c4f6a9c6ba008ce
lucabrlu <luca.breisinger@web.de>
2025-07-01 14:47:03 +0200

    Uncomment Corona.render

--- index.py
+++ index.py
@@ -146,9 +146,9 @@ import components.Mission_4.Mission_4 as Mission4
 
 #Onboarding.render()
 
-Corona.render()
+#Corona.render()
 
-#FactCheckers.render()
+FactCheckers.render()
 
 #AiFakeNews.render()
 
